60c0c9cb3ff76634201efcd19c2ba227219fa656
sanjithhithub <msanjith130@gmail.com>
2024-09-19 10:27:21 +0530

    changes are in viwes and models

--- ccapi/models.py
+++ ccapi/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 
 class Employee(models.Model):
+    id = models.AutoField(primary_key=True)  # Explicitly defining auto-increment field
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
 
 class EmployeeDetails(models.Model):
     name = models.CharField(max_length=255)
