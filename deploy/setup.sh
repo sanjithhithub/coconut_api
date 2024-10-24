30ceb53fab03644623df77045fcf3df436b58d04
sanjithhithub <msanjith130@gmail.com>
2024-10-24 14:55:59 +0530

    setup file change made

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -14,7 +14,7 @@ locale-gen en_GB.UTF-8
 # Install Python, SQLite, pip, and other dependencies
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv python3-pip sqlite3 supervisor nginx git
+apt-get install -y python3-dev python3-venv python3-pip sqlite3 supervisor nginx git build-essential libpcre3-dev libssl-dev
 
 # Create project directory and clone your GitHub repo
 mkdir -p $PROJECT_BASE_PATH
@@ -24,7 +24,9 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 python3 -m venv $PROJECT_BASE_PATH/env
 
 # Install required Python packages inside the virtual environment
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+$PROJECT_BASE_PATH/env/bin/pip install --upgrade pip  # Upgrade pip
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install uwsgi  # Install uwsgi without a specific version
 
 # Run Django migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
