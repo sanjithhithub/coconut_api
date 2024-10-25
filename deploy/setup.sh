fbd15b93ee33f0ec95264d3c1c619c9925d0b34e
sanjithhithub <msanjith130@gmail.com>
2024-10-25 13:59:20 +0530

    changes in setup files

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -32,56 +32,14 @@ $PROJECT_BASE_PATH/env/bin/pip install uwsgi  # Install uwsgi without a specific
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
 # Setup Supervisor to run the uwsgi process for your project
-cat <<EOL > /etc/supervisor/conf.d/coconut_api.conf
-[program:coconut_api]
-command=$PROJECT_BASE_PATH/env/bin/uwsgi --ini $PROJECT_BASE_PATH/uwsgi.ini
-directory=$PROJECT_BASE_PATH
-user=www-data
-autostart=true
-autorestart=true
-redirect_stderr=true
-EOL
+cp $PROJECT_BASE_PATH/deploy/supervisor_coconut_api.conf /etc/supervisor/conf.d/coconut_api.conf
 
 supervisorctl reread
 supervisorctl update
 supervisorctl restart coconut_api
 
 # Setup Nginx to make your application accessible
-cat <<EOL > /etc/nginx/sites-available/coconut_api.conf
-upstream django {
-    server 127.0.0.1:8000;  # Ensure your app is listening on this port
-}
-
-server {
-    listen 80;  # Listen on port 80 for HTTP requests
-    server_name your_domain_or_ip;  # Change this to your server's domain or IP
-
-    # Serve static files
-    location /static {
-        alias /usr/local/apps/coconut_api/api/static;  # Adjust path to your static files
-    }
-
-    # Serve media files (optional)
-    location /media {
-        alias /usr/local/apps/coconut_api/api/media;  # Adjust path to your media files
-    }
-
-    location / {
-        proxy_pass http://django;  # Forward requests to the Django app
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_redirect off;
-    }
-
-    # Optional: Handle 404 errors for missing static files
-    error_page 404 /404.html;
-    location = /404.html {
-        root /usr/local/apps/coconut_api/api/static;  # Change path if needed
-    }
-}
-EOL
+cp $PROJECT_BASE_PATH/deploy/nginx_coconut_api.conf /etc/nginx/sites-available/coconut_api.conf
 
 # Check if the symbolic link already exists and remove it if necessary
 if [ -L /etc/nginx/sites-enabled/coconut_api.conf ]; then
