30c161afe8cb5b2e26e00e2cda1bb5eb353e09d4
sanjithhithub <msanjith130@gmail.com>
2024-10-25 14:27:04 +0530

    changes made in deploy

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e  # Exit immediately if a command exits with a non-zero status
+set -e
 
 # Set to URL of your git repo.
 PROJECT_GIT_URL='https://github.com/sanjithhithub/coconut_api.git'
@@ -11,20 +11,14 @@ PROJECT_BASE_PATH='/usr/local/apps/coconut_api/api/'
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install dependencies
+# Install Python, SQLite, pip, and other dependencies
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv python3-pip sqlite3 supervisor nginx git build-essential libpcre3-dev libssl-dev
 
 # Create project directory and clone your GitHub repo
 mkdir -p $PROJECT_BASE_PATH
-if [ ! -d "$PROJECT_BASE_PATH/.git" ]; then
-    git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
-else
-    echo "Project already cloned, pulling latest changes..."
-    cd $PROJECT_BASE_PATH
-    git pull
-fi
+git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
 # Setup Python virtual environment
 python3 -m venv $PROJECT_BASE_PATH/env
@@ -37,25 +31,25 @@ $PROJECT_BASE_PATH/env/bin/pip install uwsgi  # Install uwsgi without a specific
 # Run Django migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
-# Collect static files
-$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py collectstatic --noinput
-
-# Setup Supervisor
+# Setup Supervisor to run the uwsgi process for your project
 cp $PROJECT_BASE_PATH/deploy/supervisor_coconut_api.conf /etc/supervisor/conf.d/coconut_api.conf
+
 supervisorctl reread
 supervisorctl update
 supervisorctl restart coconut_api
 
-# Setup Nginx
+# Setup Nginx to make your application accessible
 cp $PROJECT_BASE_PATH/deploy/nginx_coconut_api.conf /etc/nginx/sites-available/coconut_api.conf
 
-# Create symbolic link for Nginx configuration
+# Check if the symbolic link already exists and remove it if necessary
 if [ -L /etc/nginx/sites-enabled/coconut_api.conf ]; then
     rm /etc/nginx/sites-enabled/coconut_api.conf
 fi
+
+# Create a new symbolic link for the Nginx configuration
 ln -s /etc/nginx/sites-available/coconut_api.conf /etc/nginx/sites-enabled/coconut_api.conf
 
-# Restart Nginx
+# Restart Nginx to apply changes
 systemctl restart nginx.service
 
-echo "Deployment of coconut API completed."
+echo "Deployment of coconut API completed"
