9a3f9ba16fc61bb5bbd192783a261e71d210e493
sanjithhithub <msanjith130@gmail.com>
2024-10-26 13:24:16 +0530

    changes in supervisor

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -14,7 +14,9 @@ locale-gen en_GB.UTF-8
 # Install Python, SQLite3, and pip
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
+
+# Install necessary packages, including build-essential for compiling C extensions
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git build-essential libssl-dev zlib1g-dev libpcre3 libpcre3-dev
 
 # Create project directory and clone the repository
 mkdir -p $PROJECT_BASE_PATH
@@ -23,10 +25,17 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 # Set up Python virtual environment
 python3 -m venv $PROJECT_BASE_PATH/env
 
-# Install required Python packages inside the virtual environment
-$PROJECT_BASE_PATH/env/bin/pip install --upgrade pip
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Activate the virtual environment
+source $PROJECT_BASE_PATH/env/bin/activate
 
+# Upgrade pip and install required Python packages inside the virtual environment
+$PROJECT_BASE_PATH/env/bin/pip install --upgrade pip
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+
+# Explicitly install uWSGI version that is compatible with your Python version
+# For Python 3.8 and above, you can use the latest version
+# Modify the version number according to your Python version
+$PROJECT_BASE_PATH/env/bin/pip install uwsgi
 # Run Django migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
@@ -38,16 +47,8 @@ supervisorctl restart coconut_api
 
 # Setup Nginx to make the coconut_api accessible
 cp $PROJECT_BASE_PATH/deploy/nginx_coconut_api.conf /etc/nginx/sites-available/coconut_api.conf
-
-# Remove the default configuration file if it exists
-if [ -f /etc/nginx/sites-enabled/default ]; then
-    rm /etc/nginx/sites-enabled/default
-fi
-
-# Link your configuration file
+rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/coconut_api.conf /etc/nginx/sites-enabled/coconut_api.conf
-
-# Restart Nginx service
 systemctl restart nginx.service
 
 echo "Deployment of coconut API completed successfully!"
